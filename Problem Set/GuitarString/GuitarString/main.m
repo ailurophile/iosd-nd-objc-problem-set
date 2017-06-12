e45eb83e797f9a03a0b2dcc6428884e5cfe86e50
ailurophile <lisalitchfield@aim.com>
2017-06-12 16:38:33 -0700

    Solved Problem 7 - GuitarString

--- Problem Set/GuitarString/GuitarString/main.m
+++ Problem Set/GuitarString/GuitarString/main.m
@@ -13,10 +13,16 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         // a. Create an instance of the class GuitarString
+        GuitarString *guitarString = [[GuitarString alloc] init];
         // b. Create an NSError
+        NSError *error;
         // c. Call the method pluck(velocity: Float)
+        [guitarString pluck:12.0 error: &error];
         // d. Check if an error was returned
         // e. Log an error if one was returned
+        if (error != nil){
+            NSLog(@"Error domain: %@ and code %ld", error.domain, (long)error.code);
+        }
 
     }
     return 0;
